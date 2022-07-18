a58229396926664590211b09c1a76b261bc30deb
Paulo <paulofs10.sea@gmail.com>
2022-07-18 00:31:34 -0300

    add move to character

---
+++ terraria/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 10f;
+
+    Rigidbody2D myRigidBody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run(){
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+         
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody2D.velocity.y);
+        myRigidBody2D.velocity = playerVelocity;
+    }
+}
